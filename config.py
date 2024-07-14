4922ece8f3cf2bf38c4ed5d0315d645843f36914
Somoydip Chakraborty <157705000+somoydipc@users.noreply.github.com>
2024-07-14 12:24:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
+
+
 # devggn
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGODB_CONNECTION_STRING", "mongodb+srv://ggn:ggn@ggn.upuljx5.mongodb.net/?retryWrites=true&w=majority&appName=ggn")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-SESSION = getenv("SESSION", "")
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "24051078"))
+API_HASH = getenv("API_HASH", "5fbd3d24b82a74fb1b0d21fa1d1ce70b")
+BOT_TOKEN = getenv("BOT_TOKEN", "7407151388:AAFz3qluZMUYfMDFYDEYrcvuKWhcrXUpGkk")
+OWNER_ID = int(getenv("OWNER_ID", "6220013615"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://zennie713:hindi2992@cluster0.uzztvri.mongodb.net")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002227226447"))
+FORCESUB = getenv("FORCESUB", "wrgert")
